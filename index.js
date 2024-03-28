62a00adbe2213556061908abf8dbcff4db53b2a5
r.podobniy <r.podobniy@firstswgroup.com>
2024-03-28 19:14:59 +0300

    new commit

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ const PORT = 5000;
 const cors = require('cors');
 const index = require('./routes/index');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 
 app.use(cors());
 app.use(express.json());
 app.use('/', index);
 
-const start = () => {
-    try {
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
-    } catch (e) {
-        console.log(e);
-    }
+const options = {
+	key: fs.readFileSync('./config/ssl/private.key'),
+	cert: fs.readFileSync('./config/ssl/certificate.crt'),
 };
 
-start();
+https.createServer(options, app).listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+});
+
