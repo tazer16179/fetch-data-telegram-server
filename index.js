9cb216f43da0cfae33cbf1735a7f8d6d98eb38dd
r.podobniy <r.podobniy@firstswgroup.com>
2024-05-31 00:23:15 +0300

    commit where i fix cookie

--- index.js
+++ index.js
@@ -5,10 +5,24 @@ const index = require('./routes/index');
 const app = express();
 const https = require('https');
 const fs = require('fs');
+const session = require('express-session');
+const config = require('../config/config.json');
 
 app.use(cors());
 app.use(express.json());
 app.use('/', index);
+
+app.use(
+	session({
+		secret: config.telegram.secretKey, // Используйте секретный ключ из переменных окружения
+		resave: false,
+		saveUninitialized: true,
+		cookie: {
+			sameSite: 'Strict', // или 'Lax', или 'None'
+			secure: true, // Используйте secure: true для https
+		},
+	})
+);
 
 const options = {
 	key: fs.readFileSync('./config/ssl/private.key'),
